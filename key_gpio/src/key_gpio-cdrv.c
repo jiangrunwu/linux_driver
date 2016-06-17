ff22bfe4a7a895aec2c5ddc0648eaff1d427a7a0
jiangrunwu <jiangrunwu@163.com>
2016-06-17 21:16:43 +0800

    reset the key_gpio-cdrv.c

--- key_gpio/src/key_gpio-cdrv.c
+++ key_gpio/src/key_gpio-cdrv.c
@@ -1,22 +1,24 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
+#include <linux/sched.h>
+#include <linux/pm.h>
+#include <linux/slab.h>
+#include <linux/sysctl.h>
+#include <linux/proc_fs.h>
+#include <linux/delay.h>
+#include <linux/platform_device.h>
+#include <linux/input.h>
+#include <linux/gpio_keys.h>
+#include <linux/workqueue.h>
+#include <linux/gpio.h>
+#include <linux/of_platform.h>
+#include <linux/of_gpio.h>
+#include <linux/spinlock.h>
 
-#include <linux/gpiolib.h>
-
-
-
-
-static struct input_dev *key_16_dev;
-
-
-
-static void key_interrupt(int irq, void *dummy, struct pt_regs *fp){
-
-
-
-    input_report_key();
-
-
-}
-
+#include <linux/of.h>
 
 
 
@@ -46,12 +48,18 @@ struct gpio_key_data {
 };
 
 
-static void gpio_keys_irq_timer(unsigned long arg){
 
+static irqreturn_t gpio_keys_irq(int irq, void *dev_id){
 
+    struct gpio_key_data *key_data = (struct gpio_key_data *)dev_id;
+        
+    schedule_work(&key_data->work);
 
+    return IRQ_HANDLED;
+    
 
 }
+
 
 
 static void gpio_keys_work_queue_func(struct work_struct *work){
@@ -60,7 +68,7 @@ static void gpio_keys_work_queue_func(struct work_struct *work){
     int state = gpio_get_value_cansleep(key_data->jkey->gpio) ^ key_data->jkey->active_low;
 
 
-    input_event(key_data->input, type, gpio_key_data->jkey->code, key_data->jkey->value);
+    input_event(key_data->input, EV_KEY, key_data->jkey->code, !!state);
 
     input_sync(key_data->input);
 
@@ -70,9 +78,7 @@ static void gpio_keys_work_queue_func(struct work_struct *work){
 }
 
 
-
-
-static struct jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
+static jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
 
     struct device_node *node, *pp; 
     jansion_key_t *jkey;
@@ -100,7 +106,7 @@ static struct jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
     if(!of_get_property(pp, "gpios", NULL)){
 
         printk("find button without gpios\n");
-        return -1;
+        return ERR_PTR(-ENODEV);
     }
 
     gpio = of_get_gpio_flags(pp, 0, &flag);
@@ -110,7 +116,7 @@ static struct jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
 
         return ERR_PTR(-gpio);
     }
-    jkey = devm_kzalloc(dev, sizeof(struct jansion_key_t), GFP_KERNEL); 
+    jkey = devm_kzalloc(dev, sizeof(jansion_key_t), GFP_KERNEL); 
     if(!jkey){
 
         return ERR_PTR(-ENOMEM);
@@ -119,7 +125,7 @@ static struct jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
     jkey->gpio = gpio;
     jkey->active_low = flag & OF_GPIO_ACTIVE_LOW;
 
-    if(of_property_read_u32(pp, "linux,code", jkey->code)){
+    if(of_property_read_u32(pp, "linux,code", &jkey->code)){
 
         return ERR_PTR(-EINVAL);
     }
@@ -132,35 +138,48 @@ static struct jansion_key_t *gpio_keys_get_devtree_pdata(struct device *dev){
 }
 
 
-irqreturn_t jkey_irq_handler_t(int, void *);
 
 int key_16_probe(struct platform_device *key_device){
     struct device *dev = &key_device->dev;
 
     struct gpio_key_data *key_data; 
-    struct jansion_key jkey = NULL;
+    struct jansion_key *jkey = NULL;
     struct input_dev *input;
-    int jkey_irq;
     int ret;
+    unsigned long irqflags;
+    irq_handler_t isr;
 
+
+
+    printk("---------------------init start--------------------------\n");
 
     jkey = gpio_keys_get_devtree_pdata(dev); 
     if(IS_ERR(jkey)){
 
-        return PTR_ERR(-ENOMEM);  
+        return (-ENOMEM);  
 
     }
 
     key_data = devm_kzalloc(dev, sizeof(struct gpio_key_data), GFP_KERNEL);
     if(IS_ERR(key_data)){
 
-        return PTR_ERR(-ENOMEM);  
+        return (-ENOMEM);  
     }
+
+    printk("---------------------stage   1--------------------------\n");
     input = devm_input_allocate_device(dev);
     if(IS_ERR(input)){
 
-        return PTR_ERR(-ENOMEM);  
+        return (-ENOMEM);  
     }
+    
+
+    key_data->jkey = jkey;
+    key_data->input = input;
+    
+
+	platform_set_drvdata(key_device, key_data);
+	input_set_drvdata(input, key_data);
 
     input->dev.parent = dev;
     input->name = key_device->name; 
@@ -168,52 +187,62 @@ int key_16_probe(struct platform_device *key_device){
     input->id.product = 0x0001;
     input->id.version = 0x0100;
 
+	input_set_capability(key_data->input, EV_KEY, jkey->code);
 
-    key_data->jkey = jkey;
-    key_data->input = input;
-
+    printk("---------------------stage   2--------------------------\n");
     if(!gpio_is_valid(jkey->gpio)){
 
         return -1; 
     }
 
-    error = devm_gpio_request_one(dev, jkey->gpio, CONFIG_IN, jkey->desc);
+    printk("---------------------stage   3--------------------------\n");
+    ret = devm_gpio_request_one(dev, jkey->gpio, GPIOF_IN, jkey->desc);
+    if(ret < 0){
+        
+        return -ret;
+    
+    }
     //config the gpio into input mode
     key_data->irq = gpio_to_irq(jkey->gpio);
-    if(jkey_irq < 0){
+    
 
-        return ERR_PTR(-jkey_irq);  
+    INIT_WORK(&key_data->work, gpio_keys_work_queue_func);
+    //setup_timer(key_data->tierm, gpio_keys_irq_timer, (unsigned long)&key_data);
+
+
+    isr = gpio_keys_irq; 
+	irqflags = IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING;
+	ret = devm_request_any_context_irq(dev, key_data->irq, \
+					     isr, irqflags, key_data->jkey->desc, key_data);
+
+
+    printk("---------------------stage   4--------------------------\n");
+    ret = input_register_device(key_data->input);
+    if(ret < 0){
+    
+        return -ret;
     }
+    printk("---------------------init finished--------------------------\n");
+    
+    return 0;
 
-    INIT_WORK(key_data->work, );
-
-    setup_timer(key_data->tiemr, gpio_keys_irq_timer, (unsigned long)&key_data);
-
-
-
-    request_irq(unsigned int irq, irq_handler_t handler, unsigned long flags,
+}
 
 
 
+int key_16_remove(struct platform_device *key_device){
 
 
+    return 0;
 
+}
 
-            }
-            }
+static const struct of_device_id key_16_dt[] = {
 
-            int key_16_remove(struct platform_device *key_device){
+    { .compatible = "gpio-keys",   },
+    {},
 
-
-
-
-            }
-
-            static const struct of_match_table = key_16_dt{
-
-            { .compatible = "gpio-keys"   },
-
-            }; 
+}; 
 
 MODULE_DEVICE_TABLE(of, key_16_dt);
 
@@ -226,8 +255,8 @@ static struct platform_driver  key_16_driver = {
     .driver = {
 
         .owner =  THIS_MODULE,
-        .name = "jansion_key",
-        .of_match_table =  of_match_ptr(key_16_dt),
+        .name = "gpio-keysi",
+        .of_match_table =  key_16_dt,
 
     },
 
@@ -239,24 +268,29 @@ static struct platform_driver  key_16_driver = {
 static int __init key_16_init(void){
 
 
+    printk("---------------------init f--------------------------\n");
+    
     platform_driver_register(&key_16_driver); 
 
+    printk("---------------------init f--------------------------\n");
 
     return 0;
 }
 
-static void __exit key_16_exit(){
+static void __exit key_16_exit(void){
 
 
     platform_driver_unregister(&key_16_driver); 
 
+    printk("---------------------exit func--------------------------\n");
 }
 
 
 
 
-late_initcall();
-module_init();
+MODULE_LICENSE("GPL");
+late_initcall(key_16_init);
+module_exit(key_16_exit);
 
 
 
